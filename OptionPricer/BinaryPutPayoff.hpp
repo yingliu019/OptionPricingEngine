80e0845afb1e748bc5183b42be3ba648a1e6722c
yingliu019 <yingliu019@gmail.com>
2017-02-20 15:51:26 -0500

    Update BinaryPutPayoff.hpp

--- OptionPricer/BinaryPutPayoff.hpp
+++ OptionPricer/BinaryPutPayoff.hpp
@@ -2,8 +2,8 @@
 //  BinaryPutPayoff.hpp
 //  OptionPricer
 //
-//  Created by 刘颖 on 1/24/17.
-//  Copyright © 2017 刘颖. All rights reserved.
+//  Created by YingLiu on 1/24/17.
+//  Copyright © 2017 YingLiu. All rights reserved.
 //
 
 #ifndef BinaryPutPayoff_hpp
