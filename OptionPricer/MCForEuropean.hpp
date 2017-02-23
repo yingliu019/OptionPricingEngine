e9bfd2fad560eeee80bb245d4c7e35d57207177b
yingliu019 <yingliu019@gmail.com>
2017-02-23 15:03:41 -0500

    Update MCForEuropean.hpp

--- OptionPricer/MCForEuropean.hpp
+++ OptionPricer/MCForEuropean.hpp
@@ -2,8 +2,8 @@
 //  MCForEuropean.hpp
 //  OptionPricer
 //
-//  Created by 刘颖 on 1/25/17.
-//  Copyright © 2017 刘颖. All rights reserved.
+//  Created by YingLiu on 1/25/17.
+//  Copyright © 2017 YingLiu. All rights reserved.
 //
 
 #ifndef MCForEuropean_hpp
