63c4acbc935a3d7251fa8d0b0dde958aade4b658
yingliu019 <yingliu019@gmail.com>
2017-02-23 15:00:37 -0500

    Update HestonProcess.hpp

--- OptionPricer/HestonProcess.hpp
+++ OptionPricer/HestonProcess.hpp
@@ -2,8 +2,8 @@
 //  HestonProcess.hpp
 //  OptionPricer
 //
-//  Created by 刘颖 on 2/9/17.
-//  Copyright © 2017 刘颖. All rights reserved.
+//  Created by YingLiu on 2/9/17.
+//  Copyright © 2017 YingLiu. All rights reserved.
 //
 
 #ifndef HestonProcess_hpp
