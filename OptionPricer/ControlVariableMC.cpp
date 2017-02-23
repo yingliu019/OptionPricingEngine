710f799c8a9e1356aa62167730d9fb49955c7bff
yingliu019 <yingliu019@gmail.com>
2017-02-23 14:56:58 -0500

    Update ControlVariableMC.cpp

--- OptionPricer/ControlVariableMC.cpp
+++ OptionPricer/ControlVariableMC.cpp
@@ -2,11 +2,11 @@
 //  ControlVariableMC.cpp
 //  OptionPricer
 //
-//  Created by 刘颖 on 2/10/17.
-//  Copyright © 2017 刘颖. All rights reserved.
+//  Created by YingLiu on 2/10/17.
+//  Copyright © 2017 YingLiu. All rights reserved.
 //
 
 #include "ControlVariableMC.hpp"
 
-//针对复杂的contract，比如asian，现在先不考虑
+//e.g.: contract such as asian option
 // hw 5
