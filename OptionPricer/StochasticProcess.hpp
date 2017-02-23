573e9d92c57ab0735c702481249340b9836c2900
yingliu019 <yingliu019@gmail.com>
2017-02-23 15:08:04 -0500

    Update StochasticProcess.hpp

--- OptionPricer/StochasticProcess.hpp
+++ OptionPricer/StochasticProcess.hpp
@@ -2,8 +2,8 @@
 //  StochasticProcess.hpp
 //  OptionPricer
 //
-//  Created by 刘颖 on 2/11/17.
-//  Copyright © 2017 刘颖. All rights reserved.
+//  Created by YingLiu on 2/11/17.
+//  Copyright © 2017 YingLiu. All rights reserved.
 //
 
 #ifndef StochasticProcess_hpp
