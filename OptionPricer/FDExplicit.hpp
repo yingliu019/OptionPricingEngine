a10119e299ee3a291e637d54b0063966c45ee495
yingliu019 <yingliu019@gmail.com>
2017-02-23 15:00:19 -0500

    Update FDExplicit.hpp

--- OptionPricer/FDExplicit.hpp
+++ OptionPricer/FDExplicit.hpp
@@ -2,8 +2,8 @@
 //  FDExplicit.hpp
 //  OptionPricer
 //
-//  Created by 刘颖 on 1/24/17.
-//  Copyright © 2017 刘颖. All rights reserved.
+//  Created by YingLiu on 1/24/17.
+//  Copyright © 2017 YingLiu. All rights reserved.
 //
 
 #ifndef FDExplicit_hpp
