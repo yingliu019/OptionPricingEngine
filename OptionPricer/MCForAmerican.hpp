986c073e6b6bf9a7b6f38e6bc7fe4faf664b420e
yingliu019 <yingliu019@gmail.com>
2017-02-23 15:02:45 -0500

    Update MCForAmerican.hpp

--- OptionPricer/MCForAmerican.hpp
+++ OptionPricer/MCForAmerican.hpp
@@ -2,8 +2,8 @@
 //  MCForAmerican.hpp
 //  OptionPricer
 //
-//  Created by 刘颖 on 1/25/17.
-//  Copyright © 2017 刘颖. All rights reserved.
+//  Created by YingLiu on 1/25/17.
+//  Copyright © 2017 YingLiu. All rights reserved.
 //
 
 
