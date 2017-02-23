228972fd0da5dfcf196464edfe778d01289ae584
yingliu019 <yingliu019@gmail.com>
2017-02-23 15:04:06 -0500

    Update MeanReverseProcess.cpp

--- OptionPricer/MeanReverseProcess.cpp
+++ OptionPricer/MeanReverseProcess.cpp
@@ -2,12 +2,11 @@
 //  MeanReverseProcess.cpp
 //  OptionPricer
 //
-//  Created by 刘颖 on 2/9/17.
-//  Copyright © 2017 刘颖. All rights reserved.
+//  Created by YingLiu on 2/9/17.
+//  Copyright © 2017 YingLiu. All rights reserved.
 //
 
 #include "MeanReverseProcess.hpp"
 
 
-//这样的话，monte carlo就是完全不一样的公式，见hw5的code
-//as hw 5, without considering liquidity
+//hw 5, without considering liquidity
