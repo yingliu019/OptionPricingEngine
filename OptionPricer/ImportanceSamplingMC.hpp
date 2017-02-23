4c00204e36694c4c44b987b85e4bfb6abb88d711
yingliu019 <yingliu019@gmail.com>
2017-02-23 15:01:07 -0500

    Update ImportanceSamplingMC.hpp

--- OptionPricer/ImportanceSamplingMC.hpp
+++ OptionPricer/ImportanceSamplingMC.hpp
@@ -2,8 +2,8 @@
 //  ImportanceSamplingMC.hpp
 //  OptionPricer
 //
-//  Created by 刘颖 on 2/10/17.
-//  Copyright © 2017 刘颖. All rights reserved.
+//  Created by YingLiu on 2/10/17.
+//  Copyright © 2017 YingLiu. All rights reserved.
 //
 
 //suppose you wanted to price a deep OTM option under an intractable stochastic volatility model,
